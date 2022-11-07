745b010ababd687d7e86d47584e495830e25472c
Iniyan Natarajan <iniyannatarajan@gmail.com>
2022-11-07 17:16:25 -0500

    Organise output products in a separate dir

--- examples/AnimeDriver.jl
+++ examples/AnimeDriver.jl
@@ -2,6 +2,7 @@ ENV["JULIA_CONDAPKG_BACKEND"] = "Null" # never install Conda packages; run scrip
 
 using ArgParse
 using JSON3
+using Logging
 
 include("../src/Anime.jl")
 using .Anime
@@ -17,6 +18,9 @@ function create_parser()
         "--template", "-t"
             help = "Input CASA ANTENNA table to use as template"
             arg_type = String
+        "--outdir", "-o"
+            help = "Directory to hold output products"
+            arg_type = String
         "--clobber", "-c"
             action = :store_true
             help = "Overwrite existing arguments"
@@ -28,12 +32,26 @@ end
 # create parser
 args = create_parser()
 
+
+# Change to output directory -- all operations are performed within the output directory
+startdir = pwd()
+isdir(args["outdir"]) || mkdir(args["outdir"])
+@info("Changing working directory to $(args["outdir"])")
+cd(args["outdir"])
+# ensure argparse paths are absolute
+config = abspath(startdir, args["config"])
+template = abspath(startdir, args["template"])
+
 # load input json file
-json_string = read(args["config"], String)
+json_string = read(config, String)
 jsonpars = JSON3.read(json_string)
 
 # create an empty MS
-genms(jsonpars, args["template"], args["clobber"])
+genms(jsonpars, template, args["clobber"])
 
 # call wscean to predict visibilities
 runwsclean(jsonpars.msname, jsonpars.fitsdir, Bool(jsonpars.toggle_polmodel), jsonpars.channelgroups, jsonpars.osfactor)
+
+# Change back to original working directory
+@info("Changing working directory back to $(startdir)")
+cd(startdir)
