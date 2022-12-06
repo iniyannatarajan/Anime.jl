57f2a9c67a7e6ad7249c289beb020e5b17e151e0
Iniyan Natarajan <iniyannatarajan@gmail.com>
2022-12-06 17:30:34 -0500

    Compute parallactic angle

--- src/corruption/util.jl
+++ src/corruption/util.jl
@@ -6,6 +6,32 @@ qa = quanta()
 me = measures()
 
 function parallacticangle()
+    """
+    Compute parallactic angle for each antenna for all times
+    """
+    ra = qa.quantity(phasedir[1], "rad")
+    dec = qa.quantity(phasedir[2], "rad")
+
+    pointing = me.direction("j2000", ra, dec)
+    starttime = me.epoch("utc", qa.quantity(uniqtimes[1], "s"))
+    me.doframe(starttime)
+
+    parallacticanglematrix = zeros(size(uniqtimes)[1], nant)
+
+    for ant in 1:nant
+        x = qa.quantity(pos[1, ant], "m")
+        y = qa.quantity(pos[2, ant], "m")
+        z = qa.quantity(pos[3, ant], "m")
+        position = me.position("wgs84", x, y, z)
+        me.doframe(position)
+        sec2rad = 2*pi/(24.0*3600.0)
+        hourangle = pyconvert(Float64, me.measure(pointing, "HADEC")["m0"]["value"]) .+ (uniqtimes.-minimum(uniqtimes)).*sec2rad
+        earthradius = 6371000.0
+        latitude = asin(pos[3, ant]/earthradius)
+	parallacticanglematrix[:,ant] = atan.(sin.(hourangle).*cos(latitude), (cos(phasedir[2])*sin(latitude).-cos.(hourangle).*cos(latitude).*sin(phasedir[2])))
+    end
+
+    return parallacticanglematrix
 end
 
 function elevationangle()
@@ -28,9 +54,7 @@ function elevationangle()
         position = me.position("wgs84", x, y, z)
         me.doframe(position)
         sec2rad = 2*pi/(24.0*3600.0)
-        temppointval = me.measure(pointing, "HADEC")["m0"]["value"]
-        temppointval = pyconvert(Float64, temppointval)
-        hourangle = temppointval .+ (uniqtimes.-minimum(uniqtimes)).*sec2rad
+        hourangle = pyconvert(Float64, me.measure(pointing, "HADEC")["m0"]["value"]) .+ (uniqtimes.-minimum(uniqtimes)).*sec2rad
         earthradius = 6371000.0
         latitude = asin(pos[3, ant]/earthradius)
         elevationmatrix[:,ant] = asin.(sin(latitude)*sin(phasedir[2]).+cos(latitude)*cos(phasedir[2]).*cos.(hourangle))
