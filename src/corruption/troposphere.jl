9715c3d465a37b6752d3c1bbd11f16aadb1f399f
Iniyan Natarajan <iniyannatarajan@gmail.com>
2022-12-12 20:16:51 -0500

    Add turbulence in troposphere

--- src/corruption/troposphere.jl
+++ src/corruption/troposphere.jl
@@ -1,6 +1,9 @@
 export troposphere
 
 include(joinpath("util.jl"))
+using LazyGrids
+using LinearAlgebra
+
 const Boltzmann = 1.380649e-23
 const lightspeed = 2.99792458e8
 
@@ -91,19 +94,18 @@ function attenuate(obs::CjlObservation, atmdf::DataFrame, elevationmatrix::Array
 	    row += 1 # increment the last dimension i.e. row number
         end
     end
-    @info("Applying attenuation due to opacity... 🙆")
+    @info("Apply attenuation due to opacity... 🙆")
 end
 
 function compute_skynoise(obs::CjlObservation, atmdf::DataFrame, elevation::Array{Float64, 2}, g::HDF5.Group)
     """
     Use ATM (Pardo et al. 2001) to compute atmospheric contribution to temperature
     """
-    # needs skybrightness, opacity, elevation
-    # get unique times
-    uniqtimes = unique(obs.times)
-
     # get matrix type and size to be created -- data is a 4d array
     elemtype = typeof(obs.data[1])
+
+    # get unique times
+    uniqtimes = unique(obs.times)
 
     transmission = compute_transmission(obs, atmdf, elevation, size(uniqtimes)[1], g)
     sefdarray = zeros(Float64, obs.numchan, size(uniqtimes)[1], size(obs.stationinfo)[1])
@@ -141,19 +143,22 @@ function compute_skynoise(obs::CjlObservation, atmdf::DataFrame, elevation::Arra
         g["skynoise"] = skynoise
     end
 
-    @info("Applying tropospheric noise... 🙆")
+    @info("Apply tropospheric noise... 🙆")
 end
 
 function compute_meandelays(obs::CjlObservation, atmdf::DataFrame, elevationmatrix::Array{Float64, 2}, g::HDF5.Group)
     """
     Use ATM (Pardo et al. 2001) to compute mean delays
     """
+    # get element type to be used
+    elemtype = typeof(obs.data[1][1])
+
     # get unique times
     uniqtimes = unique(obs.times)
     ntimes = size(uniqtimes)[1]
 
     # compute time and frequency varying phase delays for each station
-    phasedelays = zeros(Float32, obs.numchan, ntimes, size(obs.stationinfo)[1])
+    phasedelays = zeros(elemtype, obs.numchan, ntimes, size(obs.stationinfo)[1])
     for ant in 1:size(obs.stationinfo)[1]
         # get delta path length
 	deltapathlengthvec = obs.yamlconf["troposphere"]["wetonly"] ? atmdf[atmdf.Station .== obs.stationinfo.station[ant],:].Wet_disp + 
@@ -184,15 +189,91 @@ function compute_meandelays(obs::CjlObservation, atmdf::DataFrame, elevationmatr
         end
     end
 
-    @info("Applying mean delays due to troposphere... 🙆")    
+    @info("Apply mean delays due to troposphere... 🙆")    
 
 end
 
-function compute_turbulent_phases()
+function compute_turbulence(obs::CjlObservation, atmdf::DataFrame, elevationmatrix::Array{Float64, 2}, g::HDF5.Group)
     """
     Use ATM (Pardo et al. 2001) to compute turbulent phases
     """
-    # needs elevation
+    beta::Float64 = 5/3 # power law index
+
+    # get element type to be used
+    elemtype = typeof(obs.data[1][1])
+
+    # get unique scan numbers
+    uniqscans = unique(obs.scanno)
+
+    nant::Int64 = size(obs.stationinfo)[1] # get nant
+
+    # loop through each scan
+    row = 1 # variable to index obs.data array
+    for scan in uniqscans
+        # compute ideal ntimes per scan
+        actualtscanvec = unique(getindex(obs.times, findall(obs.scanno.==scan)))
+        idealtscanvec = collect(first(actualtscanvec):obs.exposure:last(actualtscanvec))
+        idealtscanveclen = length(idealtscanvec)
+
+	(xg, yg) = ndgrid(1:idealtscanveclen, 1:idealtscanveclen)
+	offset_idealtscanvec = idealtscanvec .- first(idealtscanvec)
+
+        # create 4d array to hold G-Jones terms per time per station
+        turbulence_phasedelays = zeros(Float64, obs.numchan, idealtscanveclen, size(obs.stationinfo)[1]) # nchan x ntimes x nant
+
+        for ant in 1:nant
+	    structD = (offset_idealtscanvec ./ obs.stationinfo.ctime_sec[ant]) .^ beta # compute structure function
+	    autocorrC = abs.(0.5 .* (last(structD) .- structD)) # compute autocorrelation function, clipped at largest mode
+
+	    # compute covariance matrix
+	    indices = abs.(xg'.-yg').+1
+	    covmatS = zeros(typeof(first(autocorrC)), idealtscanveclen, idealtscanveclen)
+	    for ii in 1:idealtscanveclen
+		for jj in 1:idealtscanveclen
+		    covmatS[ii, jj] = autocorrC[indices[ii,jj]]
+	        end
+            end
+	    L = Array(cholesky(covmatS).L) # Cholesky factorise the covariance matrix
+	    timeseries = [sum(L[ii,:] .* randn(obs.rngtrop, Float64, idealtscanveclen)) for ii in 1:idealtscanveclen]
+
+	    # populate turbulence_phasedelays
+	    for t in 1:idealtscanveclen
+		for chan in 1:obs.numchan
+		    if chan == 1
+			turbulence_phasedelays[chan, t, ant] = sqrt(1/sin(elevationmatrix[t, ant])) * timeseries[t]
+		    else
+			turbulence_phasedelays[chan, t, ant] = turbulence_phasedelays[1, t, ant] * obs.chanfreqvec[chan]/first(obs.chanfreqvec)
+		    end
+		end
+	    end
+        end
+
+        # loop over time/row and apply gjones terms corresponding to each baseline
+        for t in 1:idealtscanveclen
+            currenttime = idealtscanvec[t]
+            if currenttime in actualtscanvec
+                # read all baselines present in a given time
+                ant1vec = getindex(obs.antenna1, findall(obs.times.==currenttime))
+                ant2vec = getindex(obs.antenna2, findall(obs.times.==currenttime))
+                for (ant1,ant2) in zip(ant1vec, ant2vec)
+                    for chan in 1:obs.numchan
+                        obs.data[:,:,chan,row] .*= exp((turbulence_phasedelays[chan, t, ant1+1]-turbulence_phasedelays[chan, t, ant2+1])*im) 
+                    end
+                    row += 1 # increment obs.data last index i.e. row number
+                end
+            else
+                continue
+            end
+        end
+
+	g["scan$(scan)_turbulence_phasedelays"] = turbulence_phasedelays
+
+    end
+
+    # add datatype attribute
+    attributes(g)["datatype"] = string(typeof(read(g[keys(g)[1]])))
+
+    @info("Introduce turbulence in the troposphere... 🙆")
 end
 
 function troposphere(obs::CjlObservation)
@@ -221,8 +302,11 @@ function troposphere(obs::CjlObservation)
     # skynoise
     obs.yamlconf["troposphere"]["skynoise"] && @time compute_skynoise(obs, atmdf, elevationmatrix, g)
 
-    # skynoise
+    # meandelays
     obs.yamlconf["troposphere"]["meandelays"] && @time compute_meandelays(obs, atmdf, elevationmatrix, g)
+
+    # turbulence
+    obs.yamlconf["troposphere"]["turbulence"] && @time compute_turbulence(obs, atmdf, elevationmatrix, g)
 
     # close h5 file
     close(fid)
