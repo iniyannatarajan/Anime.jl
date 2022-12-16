848530c9418c67bd5cec8289143ca4c74bf4e021
Iniyan Natarajan <iniyannatarajan@gmail.com>
2022-12-16 11:14:15 -0500

    Update thermal noise computation and record rms and actual realisation

--- src/corruption/noise.jl
+++ src/corruption/noise.jl
@@ -7,12 +7,17 @@ function thermalnoise(obs::CjlObservation)
     # get matrix type and size to be created -- data is a 4d array
     elemtype = typeof(obs.data[1])
 
+    # get unique times
+    uniqtimes = unique(obs.times)
+    ntimes = size(uniqtimes)[1]
+    
     # open h5 file for writing
     fid = h5open(obs.yamlconf["hdf5corruptions"], "r+")
     g = create_group(fid, "thermalnoise")
     attributes(g)["desc"] = "Numerical values of thermal noise corruptions added to data"
     attributes(g)["dims"] = "stokes x nchan x ntimes_per_baseline (for all scans)"
 
+    #=
     # get ant1 and ant2 vectors with unique elements
     uniqant1 = unique(obs.antenna1)
     uniqant2 = unique(obs.antenna2)
@@ -34,6 +39,33 @@ function thermalnoise(obs::CjlObservation)
                 g["baseline_$(obs.stationinfo.station[a1+1])-$(obs.stationinfo.station[a2+1])"] = thermalvec #reduce((x,y) -> cat(x, y, dims=3), thermalvec)
 	    end
 	end
+    end
+    =#
+
+    # compute thermal noise and add to data
+    thermalnoiserms = zeros(elemtype, size(obs.data))
+    thermalnoise = zeros(elemtype, size(obs.data))
+    row = 1
+    for t in 1:ntimes # no. of unique times
+        # read all baselines present in a given time
+        ant1vec = getindex(obs.antenna1, findall(obs.times.==uniqtimes[t]))
+        ant2vec = getindex(obs.antenna2, findall(obs.times.==uniqtimes[t]))
+        for (ant1,ant2) in zip(ant1vec, ant2vec)
+            for chan in 1:obs.numchan
+                thermalnoiserms[:, :, chan, row] .= (1/obs.yamlconf["correff"]) * sqrt((obs.stationinfo.sefd_Jy[ant1+1]*obs.stationinfo.sefd_Jy[ant2+1])/(2*obs.exposure*obs.chanwidth))
+                thermalnoise[:, :, chan, row] .= thermalnoiserms[:, :, chan, row]*randn(obs.rngcorrupt, elemtype, 2, 2) # thermal noise is polarized
+                obs.data[:, :, chan, row] += thermalnoise[:, :, chan, row]
+            end
+            row += 1 # increment the last dimension i.e. row number
+        end
+    end
+
+    # write to h5 file
+    if !(haskey(g, "thermalnoiserms"))
+        g["thermalnoiserms"] = thermalnoiserms
+    end
+    if !(haskey(g, "thermalnoise"))
+        g["thermalnoise"] = thermalnoise
     end
 
     # add datatype attribute
