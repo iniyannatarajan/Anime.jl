a9e6b538ccb3f9abef3273d9ce0ce95a59f8abb1
Iniyan Natarajan <iniyannatarajan@gmail.com>
2024-04-19 11:45:33 -0300

    Update tests

--- test/testloadobs.jl
+++
@@ -1,8 +0,0 @@
-@testset "Load MS" begin
-    y = YAML.load_file("data/config1.yaml", dicttype=Dict{String,Any}) # sample dict to test loadms()
-
-    @inferred loadms(y["msname"], y["stations"], Int(y["corruptseed"]), Int(y["troposphere"]["tropseed"]), y["troposphere"]["wetonly"], y["correff"], 
-    y["troposphere"]["attenuate"], y["troposphere"]["skynoise"], y["troposphere"]["meandelays"], y["troposphere"]["turbulence"], 
-    y["instrumentalpolarization"]["visibilityframe"], y["instrumentalpolarization"]["mode"], y["pointing"]["interval"], y["pointing"]["scale"], y["pointing"]["mode"], y["stationgains"]["mode"], 
-    y["bandpass"]["bandpassfile"], delim=",", ignorerepeated=false)
-end
